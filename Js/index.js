2b858698a2dfa84a34d2fbed9d31044925eb3b03
Anandhakumar <anandhaanavamani@gmail.com>
2026-07-22 09:10:27 +0530

    condtionla

--- Js/index.js
+++ Js/index.js
@@ -100,7 +100,7 @@
 
 // BODMAS
 // bracket o ->of D-div m-multiplication A-addition s- subs
-console.log((10+2)/2*3+5+(6+20));
+// console.log((10+2)/2*3+5+(6+20));
 // 12/2*3+5+26
 // 6*3+5+26
 // 18+5+26
@@ -170,24 +170,167 @@ console.log((10+2)/2*3+5+(6+20));
 // }
 
 // without parameter
-function greet(){
-    console.log("Welcome to Functions");
+// function greet(){
+//     console.log("Welcome to Functions");
     
+// }
+// greet()
+// greet()
+// greet()
+// greet()
+// greet()
+// greet()
+// // arrow
+// const arrow = ()=>console.log("Arrow is working")
+// arrow()
+// // IIFI(Immeditate involked function expression)
+// const all = (function(){
+//     console.log("Welcome to IIFI");
+    
+// });
+
+// all();
+// // Ananoums
+// let great = function(){
+//     console.log("Welcome to Ananoums");
+    
+// }
+
+// // great()
+// const add = (a,b)=>console.log(a+b)
+
+
+// add(90,60)
+// add(80,60)
+
+
+// const multi = (a,b)=>console.log(a*b);
+
+// multi(10,7)
+
+
+// const divs = (a,b)=>console.log(a/b);
+
+// divs(4,2)
+
+// const sub = (a,b)=>console.log(a-b);
+// sub(15,10)
+
+
+// const great = (a,b)=>console.log(a>b);
+
+// great(10,5)
+
+
+
+// return
+// const add = (x,y)=>{return x+y}
+
+
+// // let total = add(10,5)
+// console.log(add(10,5));
+
+// // callback function
+// const avg =(tot)=>{
+//     let average = tot/3
+//     return average
+// }
+// const total = (s1,s2,s3)=>{
+//     const res = s1+s2+s3
+//     const stuAvg = avg(res)
+//     return stuAvg
+// }
+
+// console.log(total(63,71,89));
+
+//conditonal statements
+// if
+// let pass = 123
+// // if(pass==1234){
+// //     console.log("Mobile unlocked");    
+// // }
+// // if else
+// //     if(pass==1234){
+// //     console.log("Mobile unlocked");    
+// // }else{
+// //     console.log("Password invalid");
+    
+// // }
+// // age<13 -> child age>13 age<19-> teenager age>19-> adult
+// else if
+// let age = 63
+// if(age<13){
+//     console.log("Child");    
+// }
+// else if(age>19 && age<60){
+//     console.log("Adult");
+    
+// }
+// else if(age>60){
+//     console.log("Senior Citizens");
+    
+// }
+// else{
+//     console.log("Teenager");
+    
+// }
+
+// nested if
+// let player = "completed"
+
+// if(player=="completed"){
+//     if(player=="completed"){
+//         if(player=="completed"){
+//             console.log("Players is on Level 3");            
+//         }
+//     }
+// }
+
+// let sslc = "Fail"
+// let hsc = "pass"
+
+// if(sslc=="Pass"){
+//     console.log("SSLC Cleared");
+//     if(hsc=="pass"){
+//         console.log("He is moving to Colledge");        
+//     }    
+// }
+
+// switch
+// >,<,=,>=,
+// switch(condition){
+//     case 1:
+//         // code
+//     case 2:
+//         // code
+//     case 3:
+//         //code
+//     default:
+//         //code
+// }
+
+
+let today = "Wednesday"
+
+switch(today){
+    case "Monday":
+        console.log("Today is Monday");
+        break
+    case "Tuesday":
+        console.log("Today is Tuesday");
+        break
+    case "Wednesday":
+        console.log("Today is Wednesday");
+        break
+    case "Thursday":
+        console.log("Today is Thursday");
+        break
+    case "Friday":
+        console.log("Today is Friday");
+        break
+    case "Saturday":
+        console.log("Today is Saturday");
+        break
+    default:
+        console.log("Today is Sunday");        
 }
-greet()
-greet()
-greet()
-greet()
-greet()
-greet()
-// arrow
-// IIFI
-// Ananoums
-
-
-
-
-
-
-
-
